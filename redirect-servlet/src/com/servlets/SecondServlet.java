c91ee12365b2ba5d3d756887de852ae4c35bb9a5
marko <salamanka1985@gmail.com>
2020-03-01 17:41:33 +0100

    Finish several servlet simple task

---
+++ redirect-servlet/src/com/servlets/SecondServlet.java
@@ -0,0 +1,21 @@
+package com.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+		dispatcher.forward(request, response);
+	}
+}
